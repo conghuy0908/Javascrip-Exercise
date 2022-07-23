2c685d31e79661fba03e0c366ab369170cdfd703
conghuy0908 <conghuy0908@gmail.com>
2022-07-23 11:10:14 +0700

    xuatdl

---
+++ xuatdl/xuat.js
@@ -0,0 +1,9 @@
+function xuat(){
+    alert("Hello World");
+}
+function xuat1(){
+    let dv1 = document.getElementById("dv1").innerHTML = "Hello World";
+}
+function xuat2(){
+    document.write("Hello World");
+}
